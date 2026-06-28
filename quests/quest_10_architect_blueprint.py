bbe3cd9721d13bde9774cd155bfa864ad8c52f8b
amtoha-prog <a.mtoha@alustudent.com>
2026-06-28 13:38:52 +0200

    Float numbers

---
+++ quests/quest_10_architect_blueprint.py
@@ -0,0 +1,8 @@
+# Floating-Point Numbers (float) - Numbers with decimal points.
+
+user_length = float(input("What is the length of the rectangle? "))
+user_width = float(input("What is the width of the rectangle? "))
+
+area_rectangle = user_length * user_width
+
+print(f"The area of the rectangle is {area_rectangle}")
