79a3b74796abda829a61b1352b3a8618ae6d21b0
Regan Odhiambo Ayiecho <r.ayiecho@alustudent.com>
2026-06-28 12:30:30 +0200

    Level 4: Loops - Quests 16-20 complete

---
+++ quests/quest_17_endless_staircase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Quest 17: The Endless Staircase
+# Start a counter at 0. Use a while loop that stops when count reaches 5.
+
+count = 0
+while count <= 5:
+    print(count)
+    count = count + 1
---
+++ quests/quest_19_countdown.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# Quest 19: The Countdown
+# Simulate a rocket launch. Count down from 10 to 1, then print "Blastoff!"
+
+for i in range(10, 0, -1):
+    print(i)
+print("Blastoff!")
