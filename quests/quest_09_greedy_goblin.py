e45b80cb7bf7a864c3c00c040fbf905adb5645de
amtoha-prog <a.mtoha@alustudent.com>
2026-06-28 13:12:54 +0200

    The Greedy Goblin

---
+++ quests/quest_09_greedy_goblin.py
@@ -0,0 +1,10 @@
+# The Modulo Operator (%) - This gives you the remainder of a division.
+
+gold_pieces_of_goblin = 27
+friends = 4
+
+remaning_gold = gold_pieces_of_goblin % friends
+gold_each_friend_gets = gold_pieces_of_goblin / friends
+
+
+print(f"Each friend gets {gold_pieces_of_goblin} pieces of gold and the goblin keeps {remaning_gold} pieces of gold")
