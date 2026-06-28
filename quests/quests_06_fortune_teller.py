351344e38bdc9a712e7e601d0b1ee7957ec69386
amtoha-prog <a.mtoha@alustudent.com>
2026-06-28 11:11:56 +0200

    Asking the user for input

---
+++ quests/quests_06_fortune_teller.py
@@ -0,0 +1,5 @@
+#How to ask the user a question and get their answer.
+user_answer = input("What is your name? ")
+quest =input("What is your quest? ")
+
+print(f"Hello, {user_answer}, you are currently on {quest}.")
