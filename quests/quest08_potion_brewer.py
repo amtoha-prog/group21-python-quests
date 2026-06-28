f79da7552b59d0679bc1956f1a934ada0d421e98
amtoha-prog <a.mtoha@alustudent.com>
2026-06-28 12:52:13 +0200

    Basic AC:/Users/alissss/AppData/Local/Python/pythoncore-3.14-64/python.exe c:/Users/alissss/OneDrive/Desktop/quests/group21-python-quests/quests/quest08_potion_brewer.py

---
+++ quests/quest08_potion_brewer.py
@@ -0,0 +1,9 @@
+#  Basic Arithmetic Operators (+, -, *, /)
+
+dragon_scales = 3
+each_dragon_scales = 10
+elf_tears = 5
+each_elf_tears = 3
+
+total_cost = (3*10) + (5*3)
+print(f"The total cost of the dragon scales and elf tears you need is {total_cost}.")
